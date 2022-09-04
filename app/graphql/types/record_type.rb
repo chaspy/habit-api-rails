f8e176e99190b9ab9f4b95a590f22e45654916ab
kondo takeshi <chaspy@users.noreply.github.com>
2022-09-04 14:04:18 +0900

    Set has many

--- app/graphql/types/record_type.rb
+++ app/graphql/types/record_type.rb
@@ -4,7 +4,7 @@ module Types
   class RecordType < Types::BaseObject
     field :id, ID, null: false
     field :date, GraphQL::Types::ISO8601Date, null: false
-    field :habit_id, Integer, null: false
+    field :habit, Types::HabitType, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
