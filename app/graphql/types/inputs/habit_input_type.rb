04b014cbce5b284b3e445cda11b79313cc43c86a
kondo takeshi <chaspy@users.noreply.github.com>
2022-09-04 15:53:28 +0900

    Remove input objects

--- app/graphql/types/inputs/habit_input_type.rb
+++
@@ -1,8 +0,0 @@
-module Types
-  module Inputs
-    class HabitInputType < Types::BaseInputObject
-      argument :id,    Int,    required: true
-      argument :name,  String, required: false
-    end
-  end
-end
