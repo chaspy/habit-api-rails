418cedb89c6ab4f6b4033ced303fd228cbd86f85
kondo takeshi <chaspy@users.noreply.github.com>
2022-09-04 15:00:22 +0900

    Fix field

--- app/graphql/mutations/create_habit.rb
+++ app/graphql/mutations/create_habit.rb
@@ -2,7 +2,7 @@ module Mutations
   class CreateHabit < BaseMutation
     field :habit, Types::HabitType, null: false
 
-    argument :title, String, required: true
+    argument :name, String, required: true
 
     def resolve(**params)
       habit = Habit.create!(params)
