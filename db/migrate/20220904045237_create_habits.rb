7b69e69004179e83bbde0ff8a4ff6408cc7d9709
kondo takeshi <chaspy@users.noreply.github.com>
2022-09-04 13:56:42 +0900

    set null = false

--- db/migrate/20220904045237_create_habits.rb
+++ db/migrate/20220904045237_create_habits.rb
@@ -1,7 +1,7 @@
 class CreateHabits < ActiveRecord::Migration[7.0]
   def change
     create_table :habits do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps
     end
--- db/migrate/20220904045332_create_records.rb
+++ db/migrate/20220904045332_create_records.rb
@@ -1,7 +1,8 @@
 class CreateRecords < ActiveRecord::Migration[7.0]
   def change
     create_table :records do |t|
-      t.date :date
+      t.date :date, null: false
+      t.references :habit, null: false, foreign_key: true
 
       t.timestamps
     end
